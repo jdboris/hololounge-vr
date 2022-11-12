386558644082579bfd826d3902af8bac2c28d8bc
Joe <54695266+jdboris@users.noreply.github.com>
2022-11-12 12:07:37 -0500

    Changed from command line argument to environment variable for client app path and added error on misuse

--- server/index.js
+++ server/index.js
@@ -1,41 +1,39 @@
 import express from "express";
 import path from "path";
+import * as dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 
-// appPath: The path to the
-const [appPath, staticPath] = process.argv.slice(2);
+const { CLIENT_APP_PATH, NODE_ENV } = process.env;
 
-if (!appPath) {
+if (!CLIENT_APP_PATH) {
   console.error(
-    `Error: Expected path to client app as argument 1, received: ${appPath}`
+    `Error: Expected CLIENT_APP_PATH environment variable to be path to client app, received: ${CLIENT_APP_PATH}.`
   );
   process.exit(1);
 }
 
-if (!staticPath) {
-  console.error(
-    `Error: Expected path to static files as argument 2, received: ${staticPath}`
-  );
-  process.exit(1);
+if (NODE_ENV !== "development") {
+  // Serve the static files from the React app
+  app.use(express.static(CLIENT_APP_PATH));
 }
-
-const absoluteAppPath = path.resolve(process.cwd(), appPath);
-const absoluteStaticPath = path.resolve(process.cwd(), staticPath);
-
-// Serve the static files from the React app
-app.use(express.static(absoluteStaticPath));
 
 // TODO:
 // API routes here...
 
 // Default to React app
 app.get("*", (req, res) => {
-  res.sendFile(`${absoluteAppPath}/index.html`);
+  if (NODE_ENV === "development") {
+    res.sendFile(`${path.resolve(process.cwd())}/env-error.html`);
+    return;
+  }
+
+  res.sendFile(`${path.resolve(process.cwd(), CLIENT_APP_PATH)}/index.html`);
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log("App is listening on port " + port);
-console.log(`Serving app from '${absoluteAppPath}'`);
-console.log(`Serving static files from '${absoluteStaticPath}'`);
+console.log("Listening on port " + port);
+console.log(`Serving client app from '${CLIENT_APP_PATH}'`);
