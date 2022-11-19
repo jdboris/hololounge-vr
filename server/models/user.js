3c701d6e432f5b1c8162edfbbb5183c7d5d4f3b7
Joe <54695266+jdboris@users.noreply.github.com>
2022-11-19 10:20:54 -0500

    Create user model

---
+++ server/models/user.js
@@ -0,0 +1,42 @@
+import sequelize from "../db/db.js";
+import { DataTypes } from "sequelize";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const { NODE_ENV } = process.env;
+
+const User = sequelize.define("users", {
+  strategyId: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  passwordHash: {
+    type: DataTypes.STRING,
+  },
+  givenName: {
+    type: DataTypes.STRING,
+  },
+  familyName: {
+    type: DataTypes.STRING,
+  },
+  photoUrl: {
+    type: DataTypes.STRING,
+  },
+});
+
+try {
+  if (NODE_ENV === "development") {
+    await User.sync({ alter: true });
+  } else {
+    await User.sync();
+  }
+} catch (error) {
+  console.error("Unable to create 'users' table :", error);
+}
+
+export default User;
