d51a97a03875193c363aa50469b693a8f46b5d5b
Joe <54695266+jdboris@users.noreply.github.com>
2022-11-23 10:49:52 -0500

    Start on games section

--- client/src/pages/home-page.jsx
+++ client/src/pages/home-page.jsx
@@ -1,7 +1,15 @@
 import theme from "@jdboris/css-themes/space-station";
-import { FaChevronDown } from "react-icons/fa";
+import { useState } from "react";
+import {
+  FaChevronDown,
+  FaNetworkWired,
+  FaUserAlt,
+  FaWifi,
+} from "react-icons/fa";
 
 function HomePage() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <main>
       <div
@@ -23,6 +31,50 @@ function HomePage() {
           <FaChevronDown />
         </button>
       </div>
+      <section>
+        <header>
+          <h1>Games</h1>
+        </header>
+        <main>
+          <section>
+            <ul>
+              <li
+                className={theme.card}
+                style={{ flexGrow: isOpen ? 1 : "initial" }}
+                onClick={() => setIsOpen(true)}
+              >
+                <header>Among Us VR</header>
+                <video
+                  poster="https://cdn.akamai.steamstatic.com/steam/apps/1849900/header.jpg"
+                  src="https://cdn.akamai.steamstatic.com/steam/apps/256910182/movie480_vp9.webm"
+                  onClick={(e) =>
+                    e.target.paused ? e.target.play() : e.target.pause()
+                  }
+                />
+                <ul>
+                  <li className={theme.badge}>casual</li>
+                  <li className={theme.badge}>social</li>
+                  <li className={theme.badge}>funny</li>
+                </ul>
+                <ul>
+                  <li>
+                    <FaUserAlt /> 4-10 (<FaNetworkWired /> <FaWifi />)
+                  </li>
+                </ul>
+                {isOpen && (
+                  <p>
+                    Teamwork and betrayal in space! In this VR party game,
+                    Crewmates work together to complete tasks before one or more
+                    Impostors kill everyone aboard. Experience all of the same
+                    deception and deceit as the original game, but now in your
+                    own virtual spaceship.
+                  </p>
+                )}
+              </li>
+            </ul>
+          </section>
+        </main>
+      </section>
     </main>
   );
 }
