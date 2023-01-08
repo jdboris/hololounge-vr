589116fd2d0d05aeb1d83151f9b10019e0813a26
Joe <54695266+jdboris@users.noreply.github.com>
2023-01-08 11:02:42 -0500

    Create useDebounced hook

---
+++ client/src/hooks/debounced.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+const useDebounced = (value, milliSeconds = 500) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, milliSeconds);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, milliSeconds]);
+
+  return debouncedValue;
+};
+
+export default useDebounced;
