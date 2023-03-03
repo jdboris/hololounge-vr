839d9989146f1e02934701bcd897f08caa3f0476
Joe <54695266+jdboris@users.noreply.github.com>
2023-03-03 13:50:38 -0500

    Change service worker to network-first, and tweak serving+caching

--- server/index.js
+++ server/index.js
@@ -87,28 +87,20 @@ app.use(nocache());
 
 if (NODE_ENV !== "development") {
   // Serve the static files from the React app
-  app.use(
-    express.static(path.resolve(__dirname, CLIENT_APP_PATH), {
-      lastModified: false,
-      etag: false,
-    })
-  );
+  app.use(express.static(path.resolve(__dirname, CLIENT_APP_PATH)));
 }
 
 app.get(/.*/, (req, res) => {
   if (NODE_ENV === "development") {
-    res.sendFile(`${path.resolve(__dirname)}/env-error.html`, {
-      lastModified: false,
-      etag: false,
-    });
+    res.sendFile(`${path.resolve(__dirname)}/env-error.html`);
     return;
   }
 
   res.sendFile(`${path.resolve(__dirname, CLIENT_APP_PATH)}/index.html`, {
     // NOTE: Disable theses headers to allow App Engine's own caching to work correctly
     // Source: https://stackoverflow.com/a/64848707
-    lastModified: false,
-    etag: false,
+    // lastModified: false,
+    // etag: false,
   });
 });
 
