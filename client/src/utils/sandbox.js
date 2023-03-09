0358bd7d39813eb072782fa93b4baf9f3e621912
Joe <54695266+jdboris@users.noreply.github.com>
2023-03-09 13:44:35 -0500

    Turn SANDBOX_MODE into an environment variable

--- client/src/utils/sandbox.js
+++ client/src/utils/sandbox.js
@@ -1,4 +1,4 @@
-export const SANDBOX_MODE = false;
+export const SANDBOX_MODE = process.env.REACT_APP_SANDBOX_MODE == "true";
 export const SANDBOX_BOOKING_DATA = {
   bookingStations: [],
   startTime: new Date(),
