a048adf20a59b107b86c06dc4e469beb8b995502
Joe <54695266+jdboris@users.noreply.github.com>
2023-03-13 12:42:15 -0400

    Develop /info section

--- client/src/pages/info-page.jsx
+++ client/src/pages/info-page.jsx
@@ -8,42 +8,45 @@ function InfoPage() {
   return (
     <div className={theme.infoPage}>
       <main>
-        <div>
+        <figure>
           <img src="/img/interior-shot.jpg" />
-        </div>
+        </figure>
         <aside>
-          <h4>Welcome to HoloLounge!</h4>
           <div>
+            <h4>Welcome to HoloLounge!</h4>
+            Kick back, relax, and have some fun in virtual reality. Alone or
+            with friends, enjoy a casual and comfortable experience at your own
+            pace.
+          </div>
+          <div>
+            <div className={theme.h4}>How?</div>
+            <ol>
+              <li>
+                <Link
+                  to="/booking"
+                  onClick={(e) => e.preventDefault() || navigate("/booking")}
+                >
+                  Book a VR station
+                </Link>{" "}
+                online or in-store
+              </li>
+              <li>Check in when it's time to start</li>
+              <li>Put on your VR headset and enjoy!</li>
+            </ol>
             <span>
-              Here you can kick back, relax, experience virtual reality alone or
-              with friends.
+              Visit the <Link to="/help">HELP</Link> page for more info.
             </span>
           </div>
-          <div className={theme.h4}>How?</div>
-          <ol>
-            <li>
-              <Link
-                to="/booking"
-                onClick={(e) => e.preventDefault() || navigate("/booking")}
-              >
-                Book a VR station
-              </Link>{" "}
-              online or in-store
-            </li>
-            <li>Check in when it's time to start</li>
-            <li>Put on your VR headset and enjoy!</li>
-          </ol>
-          <span>
-            Visit the <Link to="/help">HELP</Link> page for more info.
-          </span>
-          <div className={theme.h4}>Price</div>
-          ¥2,000/hour per station*
-          <small>
-            <em>* One headset per station</em>
-          </small>
-          <small>
-            <em>* Multple patrons per station is OK!</em>
-          </small>
+          <div>
+            <div className={theme.h4}>Price</div>
+            ¥2,000/hour per station*
+            <small>
+              <em>* One headset per station</em>
+            </small>
+            <small>
+              <em>* Multple guests per station is OK!</em>
+            </small>
+          </div>
         </aside>
       </main>
     </div>
