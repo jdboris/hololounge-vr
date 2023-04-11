10c0eb5db6a7413cb711aea02c9ccec4e5794213
Joe <54695266+jdboris@users.noreply.github.com>
2023-04-11 03:55:18 -0400

    Disable D and E

--- client/src/pages/booking-page.jsx
+++ client/src/pages/booking-page.jsx
@@ -342,7 +342,10 @@ export default function BookingPage() {
   }
 
   function isStationBooked(station) {
-    if (!SANDBOX_MODE && station.name == "Station A") {
+    if (
+      !SANDBOX_MODE &&
+      (station.name == "Station D" || station.name == "Station E")
+    ) {
       return true;
     }
 
