3024b24b4da1bbde8ce9076389125cebe9d2ce59
Joe <54695266+jdboris@users.noreply.github.com>
2023-04-15 23:10:41 -0400

    Add contact info to the footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,37 +1,68 @@
-import { FaFacebookSquare, FaInstagram, FaTwitter } from "react-icons/fa";
+import theme from "@jdboris/css-themes/space-station";
+import {
+  FaFacebookSquare,
+  FaHome,
+  FaInstagram,
+  FaMapMarker,
+  FaMapMarkerAlt,
+  FaMapPin,
+  FaPhoneAlt,
+  FaRegEnvelope,
+  FaTwitter,
+} from "react-icons/fa";
 import { TbMap2 } from "react-icons/tb";
 
 function Footer() {
   return (
     <footer>
-      <nav>
-        <ul>
-          <li>
-            <a href="https://goo.gl/maps/DENfphT6i7zmz3Z47" target="_blank">
-              <TbMap2 />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com/hololoungevr" target="_blank">
-              <FaInstagram />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com/HoloLounge" target="_blank">
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.facebook.com/profile?id=100090034414820"
-              target="_blank"
-            >
-              <FaFacebookSquare />
-            </a>
-          </li>
-        </ul>
+      <address>
+        <span>
+          <FaRegEnvelope />
+          <a href="mailto:contact@hololounge.jp">contact@hololounge.jp</a>
+        </span>
+        <span>
+          <FaPhoneAlt /> <a href="tel:815464645646">+81 050-5539-2069</a>
+        </span>
+        <span>
+          <a href="https://goo.gl/maps/DENfphT6i7zmz3Z47" target="_blank">
+            <FaHome />
+            <span>〒111-0033</span>{" "}
+            <span>Tokyo, Taito City, Hanakawado, 1-5-2</span>{" "}
+            <span>サテライトフジビル 10F</span>
+          </a>
+        </span>
+      </address>
+
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <a href="https://goo.gl/maps/DENfphT6i7zmz3Z47" target="_blank">
+                <TbMap2 />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/hololoungevr" target="_blank">
+                <FaInstagram />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.twitter.com/HoloLounge" target="_blank">
+                <FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/profile?id=100090034414820"
+                target="_blank"
+              >
+                <FaFacebookSquare />
+              </a>
+            </li>
+          </ul>
+        </nav>
         ©{new Date().getFullYear()} 株式会社桜月
-      </nav>
+      </div>
     </footer>
   );
 }
